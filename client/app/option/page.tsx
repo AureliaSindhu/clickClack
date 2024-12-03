ed06ad5d30299e319b0f418f78b5ddac895cdbd3
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-12-03 14:28:33 -0800

    options

--- client/app/option/page.tsx
+++ client/app/option/page.tsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import "../style.css";
-import twoByTwo from "../../public/img/2by2.png"
-import oneByFour from "../../public/img/1by4.png"
+import twoByTwo from "../../public/img/2by2(illust).png"
+import oneByFour from "../../public/img/1by4(illust).png"
 
 export default function Option() {
     return(
@@ -13,27 +13,41 @@ export default function Option() {
             <h1 className="text-3xl font-chillax text-black mb-6">Choose Your Frame</h1>
 
             <div className="flex space-x-8">
-                <div className="flex flex-col items-center bg-[var(--canvas-light)] p-10 rounded-md">
+                {/* 2x2 Frame */}
+                <div className="group relative flex flex-col items-center p-0 rounded-md h-80 transform transition-transform duration-300 hover:scale-105 hover:rotate-[2deg]">
+                    {/* Canvas-Dark Background */}
+                    <div className="absolute inset-0 bg-[var(--canvas-dark)] rounded-md transform transition-transform duration-300 group-hover:rotate-[-2deg] z-0"></div>
+                    
+                    {/* Canvas-Light Content */}
+                    <div className="relative flex flex-col items-center bg-[var(--canvas-light)] p-10 rounded-md z-10 h-full">
                     <Image 
                         src={twoByTwo} 
                         alt="2x2 Frame" 
-                        // width={120} 
-                        height={120} 
+                        height={200} 
                         className="mb-4 object-cover"
                     />
-                    <span className="text-xl font-medium text-black">2x2 Frame</span>
+                    <span className="text-xl font-medium font-chillax text-black">2x2 Frame</span>
+                    </div>
                 </div>
-                <div className="flex flex-col items-center bg-[var(--canvas-light)] p-10 rounded-md">
+                
+                {/* 1x4 Frame */}
+                <div className="group relative flex flex-col items-center p-0 rounded-md h-80 transform transition-transform duration-300 hover:scale-105 hover:-rotate-[2deg]">
+                    {/* Canvas-Dark Background */}
+                    <div className="absolute inset-0 bg-[var(--canvas-dark)] rounded-md transform transition-transform duration-300 group-hover:rotate-[2deg] z-0"></div>
+                    
+                    {/* Canvas-Light Content */}
+                    <div className="relative flex flex-col items-center bg-[var(--canvas-light)] p-10 rounded-md z-10 h-full">
                     <Image 
                         src={oneByFour} 
                         alt="1x4 Frame" 
-                        // width={120} 
-                        height={120} 
+                        height={200} 
                         className="mb-4 object-cover"
                     />
-                    <span className="text-xl font-medium text-black">1x4 Frame</span>
+                    <span className="text-xl font-medium font-chillax text-black">1x4 Frame</span>
+                    </div>
                 </div>
             </div>
+
 
         </div>
     );
