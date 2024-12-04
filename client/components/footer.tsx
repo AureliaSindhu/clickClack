490b2ea85466c17f4d2ca8ad9686e2c7222d1b58
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-12-03 16:23:45 -0800

    updates

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="fixed bottom-6 w-full text-center text-[#535366]">
+        <footer className="w-full text-center text-[#535366] z-10 pt-10">
             <p>made with several cups of americano, love 
             <a href="https://www.instagram.com/aacodee/?hl=en" target="_blank" rel="noopener noreferrer">
                 <strong> aacode</strong>
