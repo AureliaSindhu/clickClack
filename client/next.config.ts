5e9de21fbbb92bf2ec8277d44fc3da60ddc5231a
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-12-25 16:47:28 -0800

    dependencies

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/ClickClack',
+  assetPrefix: '/ClickClack',
+  // Enable static HTML export
+  output: 'export',
 };
 
 export default nextConfig;
