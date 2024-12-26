0e378d1275b4a3c5985f2c0528a390a55b00fcfc
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-12-25 17:22:52 -0800

    build issues

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: '/ClickClack',
-  assetPrefix: '/ClickClack',
-  // Enable static HTML export
-  output: 'export',
   
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
