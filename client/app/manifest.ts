552b66be4d431b859bb2c37e61fd0ed8af83edf9
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-04-04 22:23:24 -0700

    another icon

--- client/app/manifest.ts
+++ client/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#1C1C1C',
         icons: [
         {
-            src: 'light-icon.png',
+            src: '/maskable-icon.png',
             sizes: '192x192',
             type: 'image/png',
         },
         {
-            src: 'light-icon.png',
+            src: '/maskable-icon.png',
             sizes: '512x512',
             type: 'image/png',
         },
