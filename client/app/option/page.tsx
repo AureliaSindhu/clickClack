a6784e815dc84f283e501e8f958a58d76db7251d
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-04-09 13:52:25 -0700

    pages

--- client/app/option/page.tsx
+++ client/app/option/page.tsx
@@ -1,70 +1,15 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
-import Modal from "../../components/modal";
-import "../style.css";
+import { NavPages } from "../../components/navPages";
+import OptionComponent from "../../components/option";
 import { Footer } from "../../components/footer";
-import twoByTwo from "../../public/img/2by2(illust).png"
-import oneByFour from "../../public/img/1by4(illust).png"
 
 export default function Option() {
-    const router = useRouter();
-    const [showModal, setShowModal] = useState(false);
-
-    const handleTwoByTwoClick = () => {
-        router.push('/capture');
-    };
-
     return(
         <div className="flex flex-col items-center justify-center min-h-screen bg-[var(--canvas)] p-10">
-            <h1 className="text-2xl font-chillax text-black mb-6">Choose Your Frame</h1>
-
-            <div className="flex space-x-8">
-                {/* 2x2 Frame */}
-                <div 
-                    className="group relative flex flex-col items-center p-0 rounded-md h-80 transform transition-transform duration-300 hover:scale-105 hover:rotate-[2deg]"
-                    onClick={handleTwoByTwoClick}
-                >
-                    {/* Canvas-Dark Background */}
-                    <div className="absolute inset-0 bg-[var(--canvas-dark)] rounded-md transform transition-transform duration-300 group-hover:rotate-[-2deg] z-0"></div>
-                    
-                    {/* Canvas-Light Content */}
-                    <div className="relative flex flex-col items-center bg-[var(--canvas-light)] p-5 rounded-md z-10 h-full">
-                    <Image 
-                        src={twoByTwo} 
-                        alt="2x2 Frame" 
-                        height={200} 
-                        className="mb-4 object-cover"
-                    />
-                    <span className="text-sm font-medium font-chillax text-black">2x2 Frame</span>
-                    </div>
-                </div>
-                
-                <div 
-                    className="group relative flex flex-col items-center p-0 rounded-md h-80 transform transition-transform duration-300 hover:scale-105 hover:rotate-[2deg]"
-                    onClick={() => setShowModal(true)}
-                >
-                    {/* Canvas-Dark Background */}
-                    <div className="absolute inset-0 bg-[var(--canvas-dark)] rounded-md transform transition-transform duration-300 group-hover:rotate-[-2deg] z-0"></div>
-                    
-                    {/* Canvas-Light Content */}
-                    <div className="relative flex flex-col items-center bg-[var(--canvas-light)] p-5 rounded-md z-10 h-full">
-                    <Image 
-                        src={oneByFour} 
-                        alt="1x4 Frame" 
-                        height={200} 
-                        className="mb-4 object-cover"
-                    />
-                    <span className="text-sm font-medium font-chillax text-black">1x4 Frame</span>
-                    </div>
-                </div>
-            </div>
-            {showModal && <Modal message="COMING SOON" onClose={() => setShowModal(false)} />}
-            <div className="absolute bottom-0 w-full">
-                <Footer /> 
-            </div>
+            <NavPages />
+            <OptionComponent />
+            <div className="absolute bottom-0 w-full"> <Footer /> </div>
         </div>
     );
 }
--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => {
     return (
-        <footer className="w-full text-center text-[#535366] z-10 p-10">
+        <footer className="w-full text-center text-[#535366] z-10 p-4">
             <p>made with several cups of americano, love 
             <a href="https://www.instagram.com/aacodee/?hl=en" target="_blank" rel="noopener noreferrer">
                 <strong> aacode</strong>
